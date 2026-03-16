1315e71d4b6fd4b6e1c1a1ec40d2218fc57ec3f6
Md Kamal Uddin <kamalpixidev@gmail.com>
2026-03-17 05:10:59 +0800

    Update order-list.blade.php

--- resources/views/livewire/admin/order-list.blade.php
+++ resources/views/livewire/admin/order-list.blade.php
@@ -39,7 +39,7 @@
                 @forelse ($orders as $order)
                     <tr class="border-t">
                         <td class="p-2">#{{ $order->id }}</td>
-                        <td class="p-2">{{ $order->customer ? $order->customer->name : 'Guest' }}</td>
+                        <td class="p-2">{{ $order->user ? $order->user->name : 'Guest' }}</td>
                         <td class="p-2">
                             @php
                                 $statusColors = [
@@ -60,16 +60,10 @@
                         <td class="p-2">{{ $order->created_at->format('Y-m-d H:i') }}</td>
                         <td class="p-2 text-end space-x-1">
                             <!-- View -->
-                            <a href="{{ route('admin.orders.show', ['order_id' => $order->id]) }}" class="inline-flex items-center py-1 text-blue-600 hover:text-blue-800 rounded" title="View">
+                            <a href="{{ route('admin.orders.show', ['order' => $order->id]) }}" class="inline-flex items-center py-1 text-blue-600 hover:text-blue-800 rounded" title="View">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                </svg>
-                            </a>
-                            <!-- Edit -->
-                            <a href="{{ route('admin.orders.edit', ['order_id' => $order->id]) }}" class="inline-flex items-center py-1 text-green-600 hover:text-green-800 rounded" title="Edit">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2v-5M18.5 2.5a2.121 2.121 0 113 3L12 15l-4 1 1-4 9.5-9.5z" />
                                 </svg>
                             </a>
                             <!-- Delete -->
