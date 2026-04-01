9ac04ebfe7edb9dce7dff94e62d0ae2de489aac5
Md Kamal Uddin <kamalpixidev@gmail.com>
2026-04-01 10:39:35 +0800

    nerdctl

--- docker/nerdctl-start.sh
+++ docker/nerdctl-start.sh
@@ -12,9 +12,9 @@ until nerdctl compose -f "${COMPOSE_FILE}" exec -T db sh -lc 'mysqladmin ping -u
 done
 
 echo "Running setup..."
-nerdctl compose -f "${COMPOSE_FILE}" up -d setup
+nerdctl compose -f "${COMPOSE_FILE}" up setup
 
 echo "Starting application services..."
-nerdctl compose -f "${COMPOSE_FILE}" up -d app nginx queue scheduler phpmyadmin
+nerdctl compose -f "${COMPOSE_FILE}" up app nginx queue scheduler phpmyadmin
 
 echo "Done."
